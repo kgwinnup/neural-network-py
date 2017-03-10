762f0edc61268ec98acc42c6f419e62d0d99d71a
kgwinnup <kpgwinnup@gmail.com>
2017-03-10 13:15:10 -0500

    varied the hidden layers in test xor tests

--- test_xor.py
+++ test_xor.py
@@ -11,28 +11,28 @@ X = np.array(data[:,0:-1])
 Y = np.array([data[:,-1]]).T
 
 def test_xor_11():
-    hidden = [4,2]
+    hidden = [4,2,3]
     nn = NeuralNet(Y, X, hidden=hidden)
     nn.learn()
     res = nn.predict(np.array([1,1]))
     assert(round(res['yhat'][0]) == 0.0)
 
 def test_xor_00():
-    hidden = [4,2]
+    hidden = [4]
     nn = NeuralNet(Y, X, hidden=hidden)
     nn.learn()
     res = nn.predict(np.array([0,0]))
     assert(round(res['yhat'][0]) == 0.0)
 
 def test_xor_10():
-    hidden = [4,2]
+    hidden = [3]
     nn = NeuralNet(Y, X, hidden=hidden)
     nn.learn()
     res = nn.predict(np.array([1,0]))
     assert(round(res['yhat'][0]) == 1.0)
 
 def test_xor_01():
-    hidden = [4,2]
+    hidden = [2,2]
     nn = NeuralNet(Y, X, hidden=hidden)
     nn.learn()
     res = nn.predict(np.array([0,1]))
