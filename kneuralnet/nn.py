bf745b42d9fa8dc5829cd0c90e0edb5805eb92e4
kgwinnup <kpgwinnup@gmail.com>
2017-03-15 10:52:58 -0400

    refactored intance members to have underscore for *private* members

--- kneuralnet/nn.py
+++ kneuralnet/nn.py
@@ -11,13 +11,13 @@ class NeuralNet:
         @param hidden list of layers, which each integer describing how many nodes in that layer
         @param iterations, how many loops to 'learn'
         """
-        self.X = X if not normalize else self.normalize(X)
-        self.Y = Y
-        self.hidden = hidden
-        self.iterations = iterations
-        self.synapses = None
-        self.error = None
-        self.normalized = normalize
+        self._X = X if not normalize else self.normalize(X)
+        self._Y = Y
+        self._hidden = hidden
+        self._iterations = iterations
+        self._synapses = None
+        self._error = None
+        self._normalize = normalize
 
     def sigmoid(self, t):
         return 1 / (1 + np.exp(-t))
@@ -36,40 +36,40 @@ class NeuralNet:
 
     def predict(self, X):
         """given a new feature set, determine its learned dependent value"""
-        Z = self.normalize(X) if self.normalized else X
-        return {'yhat': self.forward(Z, self.synapses)[-1],
-                'mse': np.mean(map(lambda x: x**2, self.error)),
-                'normalized_X': True,
-                'layers': self.synapses}
+        Z = self.normalize(X) if self._normalize else X
+        return {'yhat': self.forward(Z, self._synapses)[-1],
+                'mse': np.mean(map(lambda x: x**2, self._error)),
+                'normalize_X': True,
+                'layers': self._synapses}
 
     def normalize(self, M):
         return (M - M.mean()) / (M.max() - M.min())
 
     def learn(self):
         synapses = []
-        synapses.append(2 * np.random.rand(self.X.shape[1], self.hidden[0]) - 1)
-        for i in range(1,len(self.hidden)):
-            synapses.append(2 * np.random.rand(self.hidden[i-1], self.hidden[i]) - 1)
-        synapses.append(2 * np.random.rand(self.hidden[-1], 1) - 1)
+        synapses.append(2 * np.random.rand(self._X.shape[1], self._hidden[0]) - 1)
+        for i in range(1,len(self._hidden)):
+            synapses.append(2 * np.random.rand(self._hidden[i-1], self._hidden[i]) - 1)
+        synapses.append(2 * np.random.rand(self._hidden[-1], 1) - 1)
 
         layers = []
         reverse = len(synapses) - 1
-        for i in range(0, self.iterations):
+        for i in range(0, self._iterations):
 
-            layers = self.forward(self.X, synapses)
+            layers = self.forward(self._X, synapses)
 
             deltas = []
             for i in range(0, len(synapses)):
                 if i == 0:
-                    self.error = (self.Y - layers[reverse - i])
-                    deltas.append( self.error * self.sigmoid_derivitive(layers[reverse - i]) )
+                    self._error = (self._Y - layers[reverse - i])
+                    deltas.append( self._error * self.sigmoid_derivitive(layers[reverse - i]) )
                 else:
                     deltas.append( deltas[i-1].dot(synapses[reverse - i + 1].T) * self.sigmoid_derivitive(layers[reverse - i]) )
 
             for i in range(0, len(synapses)):
                 if i == len(synapses) - 1:
-                    synapses[reverse - i] = synapses[reverse - i] + self.X.T.dot(deltas[i])
+                    synapses[reverse - i] = synapses[reverse - i] + self._X.T.dot(deltas[i])
                 else:
                     synapses[reverse - i] = synapses[reverse - i] + layers[reverse - i - 1].T.dot(deltas[i])
 
-        self.synapses = synapses
+        self._synapses = synapses
