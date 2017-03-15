75713835fe48c2724a4c45662dc787bfe78a9d64
kgwinnup <kpgwinnup@gmail.com>
2017-03-15 18:46:42 -0400

    updated tests

--- tests/test_xor.py
+++ tests/test_xor.py
@@ -18,43 +18,43 @@ nX = np.array(nand[:,0:-1])
 nY = np.array([nand[:,-1]]).T
 
 def test_xor_11():
-    nn = NeuralNet(Y, X, hidden=[4,2,3])
+    nn = NeuralNet(Y, X, layers=[4,1])
     nn.learn()
     res = nn.predict(np.array([1,1]))
     assert(round(res['yhat'][0]) == 0.0)
 
 def test_xor_00():
-    nn = NeuralNet(Y, X, hidden=[4])
+    nn = NeuralNet(Y, X, layers=[4,1])
     nn.learn()
     res = nn.predict(np.array([0,0]))
     assert(round(res['yhat'][0]) == 0.0)
 
 def test_xor_10():
-    nn = NeuralNet(Y, X, hidden=[3])
+    nn = NeuralNet(Y, X, layers=[4,1])
     nn.learn()
     res = nn.predict(np.array([1,0]))
     assert(round(res['yhat'][0]) == 1.0)
 
 def test_xor_01():
-    nn = NeuralNet(Y, X, hidden=[3])
+    nn = NeuralNet(Y, X, layers=[4,1])
     nn.learn()
     res = nn.predict(np.array([0,1]))
     assert(round(res['yhat'][0]) == 1.0)
 
 def test_xor_01_b1():
-    nn = NeuralNet(Y, X, hidden=[3], bias=1)
+    nn = NeuralNet(Y, X, layers=[4,1], bias=1)
     nn.learn()
     res = nn.predict(np.array([0,1]))
     assert(round(res['yhat'][0]) == 1.0)
 
 def test_nand_01():
-    nn = NeuralNet(nY, nX, hidden=[3], bias=1)
+    nn = NeuralNet(nY, nX, layers=[3,1], bias=1)
     nn.learn()
     res = nn.predict(np.array([0,1]))
     assert(round(res['yhat'][0]) == 1.0)
 
 def test_nand_11():
-    nn = NeuralNet(nY, nX, hidden=[3], bias=1)
+    nn = NeuralNet(nY, nX, layers=[3,1], bias=1)
     nn.learn()
     res = nn.predict(np.array([1,1]))
     assert(round(res['yhat'][0]) == 0.0)
